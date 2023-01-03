a32feed1ece40e9543192806f69608e1a1f51411
David407407 <darksideinthespace@gmail.com>
2023-01-02 17:52:14 -0700

    primer commit

---
+++ views/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Salón</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+
+    <h1>Hola Mundo!</h1>
+    <!-- <?php echo $contenido; ?> -->
+            
+</body>
+</html>
