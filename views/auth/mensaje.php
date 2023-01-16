597199e6c244c17b454aa9b46740317d7966e232
David407407 <darksideinthespace@gmail.com>
2023-01-16 15:53:55 -0700

    En desarrollo

--- views/auth/mensaje.php
+++
@@ -1,2 +0,0 @@
-<h1 class='nombre-pagina'>Confirma tu cuenta</h1>
-<p class='descripcion-pagina' >Hemos enviado las instrucciones para entrar a tu cuenta en tu email</p>
--- views/templates/alertas.php
+++
@@ -1,7 +0,0 @@
-<?php foreach($alertas as $key => $mensajes): // alertas es un arreglo de arreglos asi que hay que tener cuidado al referirnos a el ?> 
-    <?php foreach($mensajes as $mensaje): ?>
-        <div class="alerta <?php echo $key; ?>">
-            <?php echo $mensaje; ?> 
-        </div> 
-    <?php endforeach; ?>
-<?php endforeach; ?>
